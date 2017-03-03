d4e0528b33b53023ac3e23a2f5ecaad717e78068
Phil Toland <phil.toland@gmail.com>
2017-03-03 11:19:57 -0600

    Update to Elixir 1.4 and ex_aws 1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExometerCloudwatch.Mixfile do
   def project do
     [app: :exometer_cloudwatch,
      version: "0.1.0",
-     elixir: "~> 1.3",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
@@ -14,12 +14,12 @@ defmodule ExometerCloudwatch.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: []]
+    [extra_applications: []]
   end
 
   defp deps do
     [
-      {:ex_aws, "~> 1.0.0"},
+      {:ex_aws, "~> 1.1"},
       {:exometer_core, github: "Feuerlabs/exometer_core", branch: "master"},
       {:meck, github: "eproxus/meck", tag: "0.8.2", override: true},
       {:edown, github: "uwiger/edown", tag: "0.7", override: true}
