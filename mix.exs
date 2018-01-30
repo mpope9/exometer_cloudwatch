62505b7ee24ab473104c147518fbe285992c9836
mpope9 <mpope@skillz.com>
2018-01-29 21:07:27 -0800

    Fixing naming

--- mix.exs
+++ mix.exs
@@ -1,8 +1,8 @@
-defmodule ExometerCloudwatch.Mixfile do
+defmodule ExometerCloudwatchWithPooling.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :exometer_cloudwatch,
+    [app: :exometer_cloudwatch_with_pooling,
      version: "0.1.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
